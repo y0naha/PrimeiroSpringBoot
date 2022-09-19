db00122328781ff2924367403101dd9a1fbcdbfe
Larissa Ionafa <65693484+y0naha@users.noreply.github.com>
2022-09-18 22:49:57 -0300

    Add files via upload

---
+++ programa01/Crud_Pessoa/src/main/java/br/com/Crud_Pessoa/CrudPessoaApplication.java
@@ -0,0 +1,15 @@
+package br.com.Crud_Pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPessoaApplication.class, args);
+	}
+
+
+
+}
---
+++ programa01/Crud_Pessoa/src/test/java/br/com/Crud_Pessoa/CrudPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Crud_Pessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
